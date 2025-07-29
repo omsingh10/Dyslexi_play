bfa672ff202a8783a09ef318c9edf28fd70ef577
omsingh10 <os0106897@gmail.com>
2025-07-30 02:09:05 +0530

    human

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:lexiplay/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const human());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
